8c9220f141f25a278b1bb6993078e99e9b542e01
Alex Booker <alexbooker@fastmail.im>
2015-06-14 16:06:35 +0100

    added search function

--- runner.js
+++ runner.js
@@ -2,6 +2,7 @@ var Genius = require("./lib/index.js");
 
 var geniusClient = new Genius(process.env.GENIUS_ACCESS_TOKEN);
 
-geniusClient.getArtist("16775", function(error, artist) {
-  console.log(artist);
+geniusClient.search("Kendrick Lamar", function(error, results) {
+  console.log(error);
+  console.log(results);
 });
