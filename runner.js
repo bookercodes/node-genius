bde173990342de1c98c5c0e44614180e5dd9c66e
Alex Booker <alexbooker@fastmail.im>
2015-06-14 18:30:04 +0100

    added getAnnotation function

--- runner.js
+++ runner.js
@@ -2,7 +2,7 @@ var Genius = require("./lib/index.js");
 
 var geniusClient = new Genius(process.env.GENIUS_ACCESS_TOKEN);
 
-geniusClient.getSong("378195", function(error, song) {
+geniusClient.getAnnotation("6721370", function(error, annotation) {
   console.log(error);
-  console.log(song);
+  console.log(annotation);
 });
