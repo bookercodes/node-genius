b120865c852864ed08389fb447bdc52652328ffc
Alex Booker <alexbooker@fastmail.im>
2015-06-14 15:59:25 +0100

    added getArtist function

--- runner.js
+++ runner.js
@@ -1,3 +1,7 @@
 var Genius = require("./lib/index.js");
 
-var geniusClient = new Genius("abc");
+var geniusClient = new Genius(process.env.GENIUS_ACCESS_TOKEN);
+
+geniusClient.getArtist("16775", function(error, artist) {
+  console.log(artist);
+});
