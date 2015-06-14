c1f8a6270f7c0816314ef47043482dab5751adaa
Alex Booker <alexbooker@fastmail.im>
2015-06-14 16:10:19 +0100

    added getSong function

--- runner.js
+++ runner.js
@@ -2,7 +2,7 @@ var Genius = require("./lib/index.js");
 
 var geniusClient = new Genius(process.env.GENIUS_ACCESS_TOKEN);
 
-geniusClient.search("Kendrick Lamar", function(error, results) {
+geniusClient.getSong("378195", function(error, song) {
   console.log(error);
-  console.log(results);
+  console.log(song);
 });
