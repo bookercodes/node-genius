ec68ed43f48b736c4f725d902c9064d2208e6f3a
Alex Booker <alexbooker@fastmail.im>
2015-06-15 21:01:28 +0100

    enabled deleteAnnotation function

--- runner.js
+++ runner.js
@@ -7,29 +7,31 @@ var geniusClient = new Genius(process.env.GENIUS_ACCESS_TOKEN);
 //   console.log(annotation);
 // });
 
-// var request = {
-//   "annotation": {
-//     "body": {
-//       "markdown": "hello **cheese!**"
-//     }
-//   },
-//   "referent": {
-//     "raw_annotatable_url": "https://docs.genius.com/",
-//     "fragment": "annotation"
-//   }
-// };
+var request = {
+  "annotation": {
+    "body": {
+      "markdown": "hello **cheese!**"
+    }
+  },
+  "referent": {
+    "raw_annotatable_url": "http://genius.com/Marianne-moore-marriage-annotated",
+    "fragment": "out of respect for which"
+  }
+};
+
 
 // geniusClient.createAnnotation(request, function(error, response) {
 //   console.log(error);
 //   console.log(response);
 // });
 
-// geniusClient.deleteAnnotation("6898682", function (error, response) {
-//   console.log("error", error );
-//   console.log("response", response );
-// });
-
-geniusClient.upvoteAnnotation("6757505", function (error, response) {
+//https://genius.com/annotations/6898683
+geniusClient.deleteAnnotation("6898683", function (error, response) {
   console.log("error", error );
   console.log("response", response );
 });
+
+// geniusClient.upvoteAnnotation("6757505", function (error, response) {
+//   console.log("error", error );
+//   console.log("response", response );
+// });
