c124be67836eba446ef591f67a9d55e264530c59
Alex Booker <alexbooker@fastmail.im>
2015-06-15 21:17:47 +0100

    added unvoteAnnotation function

--- runner.js
+++ runner.js
@@ -7,18 +7,17 @@ var geniusClient = new Genius(process.env.GENIUS_ACCESS_TOKEN);
 //   console.log(annotation);
 // });
 
-var request = {
-  "annotation": {
-    "body": {
-      "markdown": "hello **cheese!**"
-    }
-  },
-  "referent": {
-    "raw_annotatable_url": "http://genius.com/Marianne-moore-marriage-annotated",
-    "fragment": "out of respect for which"
-  }
-};
-
+// var request = {
+//   "annotation": {
+//     "body": {
+//       "markdown": "hello **cheese!**"
+//     }
+//   },
+//   "referent": {
+//     "raw_annotatable_url": "http://genius.com/Marianne-moore-marriage-annotated",
+//     "fragment": "out of respect for which"
+//   }
+// };
 
 // geniusClient.createAnnotation(request, function(error, response) {
 //   console.log(error);
@@ -26,12 +25,17 @@ var request = {
 // });
 
 //https://genius.com/annotations/6898683
-geniusClient.deleteAnnotation("6898683", function (error, response) {
-  console.log("error", error );
-  console.log("response", response );
-});
+// geniusClient.deleteAnnotation("6898683", function (error, response) {
+//   console.log("error", error );
+//   console.log("response", response );
+// });
 
 // geniusClient.upvoteAnnotation("6757505", function (error, response) {
 //   console.log("error", error );
 //   console.log("response", response );
 // });
+
+geniusClient.unvoteAnnotation("5088421", function (error, response) {
+  console.log("error", error );
+  console.log("response", response );
+});
