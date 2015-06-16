942a6856f63f2435d7d9ba21983a509e766d9f29
Alex Booker <alexbooker@fastmail.im>
2015-06-16 11:18:07 +0100

    added options arg to getArtist function

--- runner.js
+++ runner.js
@@ -50,7 +50,12 @@ var geniusClient = new Genius(process.env.GENIUS_ACCESS_TOKEN);
 //   console.log("response", response );
 // });
 
-geniusClient.getArtistSongs("16775", function (error, response) {
+// geniusClient.getArtistSongs("16775", function (error, response) {
+//   console.log("error", error );
+//   console.log("response", response );
+// });
+
+geniusClient.getArtist("16775", function (error, response) {
   console.log("error", error );
   console.log("response", response );
 });
